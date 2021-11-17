56f1ae5dabc82eb9922dc49d59bbc1df38c5b04c
akangzai <1624522591@qq.com>
2021-11-17 22:56:28 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author z_
+ * @date 2021/11/17-22:43
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
